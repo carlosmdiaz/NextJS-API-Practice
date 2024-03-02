e035526f491c9dde0ce6e7a41e378f089f16f7fe
carlosmdiaz <carlosdiaz3979@gmail.com>
2024-03-02 14:44:58 -0500

    In this commit I created a new API file that will handle all the calls for an specific id. Also, I created a new folder calaled feedback inside the API folder, and moved the all feedback.js file for the api and renamed it index.js, then I moved the [feedbackId].js file inside this folder so the URL is more accurate.

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,15 +1,30 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedbackData } from "../api/feedback";
 
 
 function FeedbackPage(props) {
+    const [feedbackData, setFeedbackData] = useState(); 
+    function loadFeedbackHandler(id) {
+        fetch(`/api/feedback/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            setFeedbackData(data.feedback);
+        });
+    }
+
   return (
-    <ul>
+    <Fragment>
+        {feedbackData && <p>{feedbackData.email}</p>}
+        <ul>
         {
             props.feedbackItems.map((item) => (
-                <li key={item.id}>{item.text}</li>
+                <li key={item.id}>
+                    {item.text} <button onClick={loadFeedbackHandler.bind(null, item.id)}>Show Datils</button>
+                </li>
             ))
         }
-    </ul>
+        </ul>
+    </Fragment>
   )
 }
 
