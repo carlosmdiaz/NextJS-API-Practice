e021d279775887a1323d27185daa4133eb8d83dd
carlosmdiaz <carlosdiaz3979@gmail.com>
2024-03-02 14:09:46 -0500

    I add the getStaticProps function to prerender the the data using the get request. Also I exported the buildFeedbackPath, and extractFeedbackData functions from feedback.js in the API file. Now using an unorder list I can prerender the list of Feedbacks in the FeedbackPage.

---
+++ pages/feedback/index.js
@@ -0,0 +1,26 @@
+import { buildFeedbackPath, extractFeedbackData } from "../api/feedback";
+
+
+function FeedbackPage(props) {
+  return (
+    <ul>
+        {
+            props.feedbackItems.map((item) => (
+                <li key={item.id}>{item.text}</li>
+            ))
+        }
+    </ul>
+  )
+}
+
+export async function getStaticProps() {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedbackData(filePath);
+    return {
+        props: {
+            feedbackItems: data,
+        },
+    }
+}
+
+export default FeedbackPage;
